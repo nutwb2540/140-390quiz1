c00837e1c7c0e909534133584c5f61b3c32391ea
Wachirawit <nutautomic@gmail.com>
2018-09-17 16:08:18 +0700

    static commit

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var pgp = require('pg-promise')();
+var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+var app = express();
+
