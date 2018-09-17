0b473cd68b23fc8e74c7f3b4f70f08e6d0172635
Wachirawit <nutautomic@gmail.com>
2018-09-17 17:31:01 +0700

    about commit

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 var express = require('express');
-var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+var mysql = require('mysql')
 var app = express();
+
+var connection = mysql.createConnection({
+    host: 'www.db4free.net',
+    user: 's140390',
+    password: 'abc123**',
+    database: 'db 140390'
+});
 
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('pages/home');
 });
+
+app.get('/about', function (req, res) {
+    var name = 'Wachirawit Bumrungchua'
+    res.render('pages/about', { fullname: name});
+});
+
+app.get('/students', function (req, res) {
+
+    connection.connect()
+
+    connection.query('select * from students', function (err, rows, fields) {
+       
+        console.log('DATA : ' + rows);
+        res.render('pages/students', { students: rows })
+
+    })
+
+    connection.end()
+
+});
+
+app.get('/subjects', function (req, res) {
+
+    connection.connect()
+
+    connection.query('select * from subjects', function (err, rows, fields) {
+       
+        console.log('DATA : ' + rows);
+        res.render('pages/subjects', { subjects: rows })
+
+    })
+
+    connection.end()
+
+});
+
 
 console.log('App is running at http://localhost:8080');
 app.listen(8080);
